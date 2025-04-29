8c4fa666c57de8f625b03e9ee8b56e3f60d692a9
litetex <40789489+litetex@users.noreply.github.com>
2025-04-29 22:18:01 +0200

    Init

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 [![Latest version](https://img.shields.io/maven-central/v/net.litetex/otel-fabric-helper-extension?logo=apache%20maven)](https://mvnrepository.com/artifact/net.litetex/otel-fabric-helper-extension)
 [![Build](https://img.shields.io/github/actions/workflow/status/litetex-oss/otel-fabric-helper-extension/check-build.yml?branch=develop)](https://github.com/litetex-oss/otel-fabric-helper-extension/actions/workflows/check-build.yml?query=branch%3Adevelop)
 
-# otel-fabric-helper-extension
+# OpenTelemetry Agent Helper Extension for Fabric-Loader
 
+This [OpenTelemetry Agent extension](https://opentelemetry.io/docs/zero-code/java/agent/extensions/) helps instrumenting [FabricMC](https://github.com/FabricMC/fabric-loader).
+
+The following instrumentations are applied:
+* KnotClassLoader correctly detects changes on its classpath
+
+The project is based on [this example](https://github.com/open-telemetry/opentelemetry-java-instrumentation/tree/main/examples/extension).
 
 ## Installation
 [Installation guide for the latest release](https://github.com/litetex-oss/otel-fabric-helper-extension/releases/latest#Installation)
---
+++ otel-fabric-helper-extension/src/main/java/net/litetex/otel/ext/fabricmc/KnotClassLoaderInstrumentation.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright © 2025 ${owner} (${email})
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.litetex.otel.ext.fabricmc;
+
+import static net.bytebuddy.matcher.ElementMatchers.isMethod;
+import static net.bytebuddy.matcher.ElementMatchers.isPublic;
+import static net.bytebuddy.matcher.ElementMatchers.isStatic;
+import static net.bytebuddy.matcher.ElementMatchers.named;
+import static net.bytebuddy.matcher.ElementMatchers.not;
+import static net.bytebuddy.matcher.ElementMatchers.takesArgument;
+import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
+
+import java.net.URL;
+
+import io.opentelemetry.javaagent.bootstrap.internal.ClassLoaderMatcherCacheHolder;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
+import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.matcher.ElementMatcher;
+
+
+public class KnotClassLoaderInstrumentation implements TypeInstrumentation
+{
+	@Override
+	public ElementMatcher<TypeDescription> typeMatcher()
+	{
+		return named("net.fabricmc.loader.impl.launch.knot.KnotClassLoader");
+	}
+	
+	@Override
+	public void transform(final TypeTransformer transformer)
+	{
+		transformer.applyAdviceToMethod(
+			isMethod()
+				.and(named("addUrlFwd"))
+				.and(takesArguments(1))
+				.and(takesArgument(0, URL.class))
+				.and(isPublic())
+				.and(not(isStatic())),
+			KnotClassLoaderInstrumentation.class.getName() + "$AddUrlAdvice");
+	}
+	
+	@SuppressWarnings("unused")
+	public static class AddUrlAdvice
+	{
+		@Advice.OnMethodExit(suppress = Throwable.class)
+		public static void onExit(@Advice.This final ClassLoader loader)
+		{
+			ClassLoaderMatcherCacheHolder.invalidateAllCachesForClassLoader(loader);
+		}
+	}
+}
